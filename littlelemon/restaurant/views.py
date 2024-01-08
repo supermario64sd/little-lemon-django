28731a9e20443032a7e420efe08ec9d8323d20a7
ryan wilson <ryan.wilsonac195@gmail.com>
2024-01-08 18:12:09 -0500

    added booking endpoint

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics, permissions
+from . import serializers
+from .models import Menu, Booking
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = serializers.MenuSerializer
+
+class BookingViewSet(generics.ListCreateAPIView):
+    queryset= Booking.objects.all()
+    serializer_class= serializers.BookingSerializer
+    permission_classes= [permissions.IsAuthenticated]
